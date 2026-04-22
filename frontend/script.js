dbad3d49ec8471bfafa12c335839b895e884a307
Raunak Kumar <212152221+codebyraunak@users.noreply.github.com>
2026-04-22 20:09:22 +0530

    fixed the code

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,24 @@
+const video = document.getElementById("camera");
+const canvas = document.getElementById("canvas");
+const result = document.getElementById("result");
 
+function startCamera() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            video.srcObject = stream;
+        })
+        .catch(err => {
+            console.log("Camera error:", err);
+        });
+}
+
+function captureImage() {
+    const context = canvas.getContext("2d");
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    context.drawImage(video, 0, 0);
+
+    result.innerText = "Image Captured ";
+}
